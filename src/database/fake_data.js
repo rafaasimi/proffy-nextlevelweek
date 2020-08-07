e7ae3ac4b92b7021b4ebc1d0d989b29839c6f24d
rafaasimi <rafaasimi@hotmail.com>
2020-08-07 14:15:59 -0300

    Add SQLite DB (Insert and consult classes)

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+    {
+        name: 'Rafael Simionato',
+        avatar: 'https://avatars2.githubusercontent.com/u/32374173?s=460&u=258cef4fd54c501227706412a4eff91a13809dd5&v=4',
+        whatsapp: '1999999999',
+        bio: 'Estusiasta das melhores tecnologias de química avançada. <br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+        subject: 'Química',
+        cost: '20',
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220],
+    },
+    {
+        name: 'Daniele Evangelista',
+        avatar: 'https://avatars2.githubusercontent.com/u/32374173?s=460&u=258cef4fd54c501227706412a4eff91a13809dd5&v=4',
+        whatsapp: '1999999999',
+        bio: 'Estusiasta das melhores tecnologias de química avançada. <br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+        subject: 'Química',
+        cost: '20',
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220],
+    }
+]
